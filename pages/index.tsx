622b4a133f346fdac3574514128f21bc9b169bc0
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-10-06 14:11:10 -0300

    feat: adiciona listagem dinamica dos professores

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material'
 import type { NextPage } from 'next'
+import { Professor } from '../src/@types/professor'
 import Lista from '../src/components/Lista/Lista'
 
 //function Botao(props) {
@@ -7,9 +8,40 @@ import Lista from '../src/components/Lista/Lista'
 //}
 
 const Home: NextPage = () => {
+  const professores : Professor[] = [
+    {
+      id: 1,
+      nome: 'Professor 1',
+      foto: 'https://github.com/eduuk15.png',
+      descricao: 'descricao1',
+      valor_hora: 100
+    },
+    {
+      id: 2,
+      nome: 'Professor 2',
+      foto: 'https://github.com/eduuk15.png',
+      descricao: 'descricao2',
+      valor_hora: 100
+    },
+    {
+      id: 3,
+      nome: 'Professor 3',
+      foto: 'https://github.com/eduuk15.png',
+      descricao: 'descricao3',
+      valor_hora: 100
+    },
+    {
+      id: 4,
+      nome: 'Professor 4',
+      foto: 'https://github.com/eduuk15.png',
+      descricao: 'descricao4',
+      valor_hora: 100
+    }
+  ]
+
   return (
     <Box sx={{ backgroundColor: 'secondary.main' }}>
-      <Lista></Lista>
+      <Lista professores={professores}></Lista>
     </Box>
   )
 }
