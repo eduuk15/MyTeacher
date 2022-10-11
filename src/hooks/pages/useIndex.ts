00401def596d88abc322c79da5abcf39a77a4cad
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-10-11 14:34:30 -0300

    feat: configura request e popula professores cadastrados

--- src/hooks/pages/useIndex.ts
+++ src/hooks/pages/useIndex.ts
@@ -1,37 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Professor } from "../../@types/professor";
+import { ApiService } from "../../services/ApiService";
 
 export function useIndex() {
-    const [listaProfessores, setListaProfessores] = useState<Professor[]>([
-        {
-          id: 1,
-          nome: 'Professor 1',
-          foto: 'https://github.com/eduuk15.png',
-          descricao: 'descricao1',
-          valor_hora: 100
-        },
-        {
-          id: 2,
-          nome: 'Professor 2',
-          foto: 'https://github.com/eduuk15.png',
-          descricao: 'descricao2',
-          valor_hora: 100
-        },
-        {
-          id: 3,
-          nome: 'Professor 3',
-          foto: 'https://github.com/eduuk15.png',
-          descricao: 'descricao3',
-          valor_hora: 100
-        },
-        {
-          id: 4,
-          nome: 'Professor 4',
-          foto: 'https://github.com/eduuk15.png',
-          descricao: 'descricao4',
-          valor_hora: 100
-        }
-      ])
+    const [listaProfessores, setListaProfessores] = useState<Professor[]>([])
+
+      useEffect(() => {
+        ApiService.get('/professores').then((resposta) => {
+            setListaProfessores(resposta.data)
+        })
+      }, [])
 
       return {
         listaProfessores
