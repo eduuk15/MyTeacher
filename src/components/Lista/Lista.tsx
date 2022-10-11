fc7733c63baba39238a96adb3e3b29a15d8df19e
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-10-11 14:58:03 -0300

    feat: adiciona arquivo que isola o formatador de valor

--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
 import { Professor } from "../../@types/professor";
+import { FormatadorService } from "../../services/FormatadorService";
 import { Descricao, Foto, Informacoes, ItemLista, ListaStyled, ListaVazia, Nome, Valor } from "./Lista.style";
 
 interface ListaProps {
@@ -16,7 +17,7 @@ const Lista = (props: ListaProps) => {
                         <Foto src={professor.foto}></Foto>
                         <Informacoes>
                             <Nome>{professor.nome}</Nome>
-                            <Valor>{professor.valor_hora.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })} por hora</Valor>
+                            <Valor>{FormatadorService.valorMonetario(professor.valor_hora)} por hora</Valor>
                             <Descricao>{professor.descricao}</Descricao>
                             <Button sx={{ width: '70%' }}>Marcar Aula com Eduardo</Button>
                         </Informacoes>
---
+++ src/services/FormatadorService.ts
@@ -0,0 +1,12 @@
+export const FormatadorService = {
+    valorMonetario(valor: number): string {
+        return valor.toLocaleString(
+            'pt-BR', 
+            { 
+                minimumFractionDigits: 2,
+                style: 'currency', 
+                currency: 'BRL' 
+            }
+        );
+    }
+}
